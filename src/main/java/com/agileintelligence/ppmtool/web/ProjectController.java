e1887141aadc166b5f26bcdbc166453476294c4a
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-12 20:07:05 +0300

    MapValidationErrorService

--- src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
+++ src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,18 +1,16 @@
 package com.agileintelligence.ppmtool.web;
 
 import com.agileintelligence.ppmtool.domain.Project;
+import com.agileintelligence.ppmtool.services.MapValidationErrorService;
 import com.agileintelligence.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,18 +19,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
 
-            for(FieldError error:result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
+        if (errorMap != null) return errorMap;
 
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST) ;
-        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
