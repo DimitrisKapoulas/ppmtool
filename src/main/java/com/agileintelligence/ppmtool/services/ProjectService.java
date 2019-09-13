b18e1908c492f4ac3c0eae5f4ad1779d25063baf
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-13 20:11:41 +0300

    Delete an existing project

--- src/main/java/com/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/com/agileintelligence/ppmtool/services/ProjectService.java
@@ -34,4 +34,12 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier (String projectid) {
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+        if(project == null) {
+            throw new ProjectIdException("Cannot delete Project with ID " + projectid);
+        }
+        projectRepository.delete(project);
+    }
 }
